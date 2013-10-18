62f2d0d5872fe95765de5c42785c09c64c8bff63
Jon Uy <jonxuy@gmail.com>
2013-10-18 14:16:25 -0400

    Baseline for app to start querying for data + config.php added

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-I work...?
+<?php
+session_start();
+
+require_once "google-api-client-src/Google_Client.php";
+require_once "google-api-client-src/contrib/Google_CalendarService.php";
+
+$apiClient = new Google_Client();
+$apiClient->setUseObjects(true);
+$service = new Google_CalendarService($apiClient);
+
+if (isset($_SESSION['oauth_access_token'])) {
+  $apiClient->setAccessToken($_SESSION['oauth_access_token']);
+} else {
+  $token = $apiClient->authenticate();
+  $_SESSION['oauth_access_token'] = $token;
+}
